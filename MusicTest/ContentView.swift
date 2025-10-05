90deee3c9f92e95682dccd0333bde8c445b3c2e2
zs-andy <andy070119@outlook.com>
2025-10-05 13:06:39 +0800

    Initial Commit

---
+++ MusicTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicTest
+//
+//  Created by Andy Lyu on 10/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicTest/MusicTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTestApp.swift
+//  MusicTest
+//
+//  Created by Andy Lyu on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
